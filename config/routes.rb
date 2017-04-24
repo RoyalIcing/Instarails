e3f6cbca3b247859be1abeb84a0e914b04221beb
Patrick Smith <patrick@burntcaramel.com>
2017-04-24 12:43:44 +1000

    Add user pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :photos
   devise_for :users
+  resources :users, only: [:index, :show]
   root 'feed#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
